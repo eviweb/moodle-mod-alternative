1c58b4143e0d7592a9ae8e739227962d3511225b
Eric Villard <dev@eviweb.fr>
2015-03-05 10:41:57 +0100

    fix: $module variable is no more used from the global scope
    Signed-off-by:Eric Villard <dev@eviweb.fr>

--- autoload.php
+++ autoload.php
@@ -2,16 +2,26 @@
 
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
-$module = new stdClass();
-$module->component = '';
+/**
+ * get the current module information
+ *
+ * @return \stdClass
+ */
+function alternative_get_module_info()
+{
+    $module = new stdClass();
+    $module->component = '';
+    include __DIR__.'/version.php';
 
-require_once __DIR__.'/version.php';
+    return $module;
+}
 
 /**
  * register autoload function
  */
 spl_autoload_register(
-    function ($class) use ($module) {
+    function ($class) {
+        $module = alternative_get_module_info();
         $classname = str_replace($module->component.'\\', '', $class);
         $file = __DIR__.DIRECTORY_SEPARATOR.
             'classes'.DIRECTORY_SEPARATOR.
