f08dd915a2150ffd0ce5f2217f591b8350c06731
Eric Villard <dev@eviweb.fr>
2017-01-31 14:01:32 +0100

    release 1.3.0
    Signed-off-by: Eric Villard <dev@eviweb.fr>

--- version.php
+++ version.php
@@ -28,11 +28,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015030500;      // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2017013100;      // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2014051200;      // Requires this Moodle version
 $plugin->cron      = 0;               // Period for cron to check this module (secs)
 $plugin->component = 'mod_alternative'; // To check on upgrade, that module sits in correct place
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.2.1 (Build 2015030500)';
+$plugin->release   = '1.3.0 (Build 2017013100)';
 
 $plugin->dependencies = array();
